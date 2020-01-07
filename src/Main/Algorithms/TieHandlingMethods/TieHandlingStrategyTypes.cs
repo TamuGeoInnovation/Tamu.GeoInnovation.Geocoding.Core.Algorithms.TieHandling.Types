819c19bb04f0666528c97719c0dfa46d4362bb06
danielgoldbergtamu <daniel.goldberg@tamu.edu>
2020-01-07 14:10:27 -0600

    added a "chooseFirst" tie handling type

--- src/Main/Algorithms/TieHandlingMethods/TieHandlingStrategyTypes.cs
+++ src/Main/Algorithms/TieHandlingMethods/TieHandlingStrategyTypes.cs
@@ -1,6 +1,6 @@
 ﻿namespace USC.GISResearchLab.Geocoding.Core.Algorithms.TieHandlingMethods
 {
-    public enum TieHandlingStrategyType { Unknown, RevertToHierarchy, FlipACoin, DynamicFeatureComposition, RegionalCharacteristics, ReturnAll };
+    public enum TieHandlingStrategyType { Unknown, RevertToHierarchy, FlipACoin, DynamicFeatureComposition, RegionalCharacteristics, ReturnAll, ChooseFirstOne };
 
 
 }
